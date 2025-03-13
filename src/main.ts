173cb57be232e8e335acfe56edf4c3529ddcf7f6
Pixelzzz <sherzik99@gmail.com>
2025-03-13 23:38:25 +0500

    some test

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
+  console.log('here');
   await app.listen(port, '::', () => {
     console.log(`Server listening on [::]${port}`);
   });
